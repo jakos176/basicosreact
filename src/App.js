6af5a7213d09a4f0e16ec72caaf06423890ea430
Edu <edumartsil95@tutanota.com>
2021-03-02 21:33:40 +0100

    React con JSX

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
 
-//Sin JSX, el react puede llegar a ser muy confuso.
+//Con JSX, se debe englobar en una etiqueta general (en este caso es el div)
 function App() {
+    const empleado = {
+        nombre: 'Juan',
+        trabajo: 'Desarrollador web'
+    };
+
     return (
-        React.createElement(
-            'div',
-            null,
-            React.createElement('input', {type: 'text', value: 'Value para el input'})
-        )
+        // <React.Fragment>
+        <div className='contenedor'>
+            <h1 className='mayusculas'>Detalles de: {empleado.nombre} que trabaja en {empleado.trabajo}</h1>
+        </div>
+        // </React.Fragment>
     );
 }
 

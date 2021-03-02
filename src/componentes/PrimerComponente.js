b2a8e5b40ea03436b239c97d82f302fa1ee1d8d9
Edu <edumartsil95@tutanota.com>
2021-03-02 21:44:58 +0100

    Primer componente en React

---
+++ src/componentes/PrimerComponente.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+
+//Primera componente creado en React
+class PrimerComponente extends Component {
+    render() {
+        return <p>Desde mi primer componente</p>
+    }
+}
+
+export default PrimerComponente;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
 import reportWebVitals from './reportWebVitals';
+import PrimerComponente from "./componentes/PrimerComponente";
 
-ReactDOM.render(<App></App>, document.getElementById('root'));
+ReactDOM.render(<PrimerComponente/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

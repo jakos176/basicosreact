7ca322eb9c60d58653a8294ea41f306057cfce27
Edu <edumartsil95@tutanota.com>
2021-03-02 21:18:47 +0100

    React sin JSX parte 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 
+//Sin JSX, el react puede llegar a ser muy confuso.
 function App() {
     return (
-        React.createElement('h1',
-            {id: 'titulo', className: 'encabezado'},
-            'Hola Mundo'
+        React.createElement(
+            'div',
+            null,
+            React.createElement('input', {type: 'text', value: 'Value para el input'})
         )
     );
 }

1cae8a8003cff8592666f9b2be075f96b6dd9960
Edu <edumartsil95@tutanota.com>
2021-03-02 22:00:33 +0100

    Class component vs Functional components

---
+++ src/componentes/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//Functional component
+//Se usan cuando una class component no tiene estado,
+// no puede usar ref ni los metodos del ciclo de vida.
+const Header = () => {
+    return (
+        <h1>Nombre del sitio</h1>
+    )
+}
+
+export default Header;
--- src/componentes/PrimerComponente.js
+++ src/componentes/PrimerComponente.js
@@ -1,9 +1,15 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
+import Header from "./Header";
 
 //Primera componente creado en React
 class PrimerComponente extends Component {
     render() {
-        return <p>Desde mi primer componente</p>
+        return (
+            <div>
+                <Header/>
+                <p>Desde mi primer componente</p>
+            </div>);
+
     }
 }
 

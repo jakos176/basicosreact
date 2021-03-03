1af7f6700de9881dde298ab122a90d6c3c75f251
Edu <edumartsil95@tutanota.com>
2021-03-03 21:54:25 +0100

    Metodos de ciclo de vida de React

--- src/componentes/Aplicacion.js
+++ src/componentes/Aplicacion.js
@@ -6,18 +6,27 @@ import Footer from "./Footer";
 //Primera componente creado en React
 class Aplicacion extends Component {
 
-    render() {
-        const
-            productos = [
-                {nombre: 'Libro', precio: 200},
-                {nombre: 'Disco de musica', precio: 100},
-                {nombre: 'Reproductor de Musica', precio: 50}
-                ];
+    //Se carga el estado0
+    state = {productos: []};
 
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                productos:
+                    [
+                        {nombre: 'Libro', precio: 200},
+                        {nombre: 'Disco de musica', precio: 100},
+                        {nombre: 'Reproductor de Musica', precio: 50}
+                    ]
+            });
+        },3000);
+    }
+
+    render() {
         return (
             <div>
                 <Header titulo="Nuestra tienda Virtual"/>
-                <Productos productos={productos}/>
+                <Productos productos={this.state.productos}/>
                 <Footer/>
             </div>);
     }
